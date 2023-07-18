025f6064fc85d8255a788c50a76c48f070833ceb
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2023-07-18 10:24:52 -0500

    feat: clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+const SetUpPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetUpPage
